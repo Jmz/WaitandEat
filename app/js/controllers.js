a47474048456263855ed5d0ba52991b746ea6698
James Owers <jamesowers@gmail.com>
2015-07-16 18:17:26 +0100

    Started following through WatchandCode tutorials.

---
+++ app/js/controllers.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  
+  .controller('LandingPageController', [function() {
+
+
+  }])
+
+  .controller('WaitlistController', [function() {
+
+
+  }]);
