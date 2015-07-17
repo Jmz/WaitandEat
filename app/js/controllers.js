4e9706609a5b6a18f18c68ea010903b306d18b9e
James Owers <jamesowers@gmail.com>
2015-07-17 14:07:29 +0100

    Added send message function

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,13 +10,14 @@ angular.module('myApp.controllers', [])
   }])
 
   .controller('WaitlistController', ['$scope', '$firebase', function($scope, $firebase) {
-
-  		var partiesRef = new Firebase('https://waitandeat1234.firebaseio.com/');
+    
+  		var partiesRef = new Firebase('https://waitandeat1234.firebaseio.com/parties');
 
   		$scope.parties = $firebase(partiesRef);
 
   		$scope.newParty = { name: '', phone: '', size: '' };
 
+      // Save a new party
   		$scope.saveParty = function() {
 
   			$scope.parties.$add($scope.newParty);
@@ -24,5 +25,15 @@ angular.module('myApp.controllers', [])
   			$scope.newParty = { name: '', phone: '', size: '' };
 
   		}
+
+      // Send a text message
+      $scope.sendTextMessage = function(phoneNumber) {
+
+        var textMessageRef = new Firebase('https://waitandeat1234.firebaseio.com/textMessages');
+        var textMessages = new $firebase(textMessageRef);
+
+        textMessages.$add({phoneNumber: phoneNumber});
+
+      };
 
   }]);
