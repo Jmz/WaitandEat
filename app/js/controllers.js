405907e78f949516a1c05a81f41b4e009a941078
James Owers <jamesowers@gmail.com>
2015-07-18 07:00:11 +0100

    Added SMS sending. Added done flag. Added notified flag.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,25 +15,30 @@ angular.module('myApp.controllers', [])
 
   		$scope.parties = $firebase(partiesRef);
 
-  		$scope.newParty = { name: '', phone: '', size: '' };
+  		$scope.newParty = { name: '', phone: '', size: '', done: false, notified: 'No' };
 
       // Save a new party
   		$scope.saveParty = function() {
 
   			$scope.parties.$add($scope.newParty);
 
-  			$scope.newParty = { name: '', phone: '', size: '' };
+  			$scope.newParty = { name: '', phone: '', size: '', done: false, notified: 'No' };
 
   		}
 
       // Send a text message
-      $scope.sendTextMessage = function(phoneNumber) {
+      $scope.sendTextMessage = function(party) {
 
         var textMessageRef = new Firebase('https://waitandeat1234.firebaseio.com/textMessages');
         var textMessages = new $firebase(textMessageRef);
-
-        textMessages.$add({phoneNumber: phoneNumber});
-
+        var newTextMessage = {
+          phoneNumber: party.phone,
+          size: party.size,
+          name: party.name
+        };
+        textMessages.$add(newTextMessage);
+        party.notified = 'Yes';
+        $scope.parties.$save(party.$id);
       };
 
   }]);
